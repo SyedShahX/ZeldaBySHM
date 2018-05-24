392de343fcf79b3ee3b27796ba1d200461a80cf9
SyedShahX <38917384+SyedShahX@users.noreply.github.com>
2018-05-24 07:01:50 +0200

    Ajout code a classe arme

--- src/modele/Arme.java
+++ src/modele/Arme.java
@@ -1,5 +1,38 @@
 package modele;
 
 public class Arme {
+	protected String nom;
+	protected int ptAttaque;
+	protected int pvArme;
+	
+	public Arme(String nom,int ptAttaque,int pvArme) {
+		this.nom=nom;
+		this.ptAttaque=ptAttaque;
+		this.pvArme=pvArme;
+	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getPtAttaque() {
+		return ptAttaque;
+	}
+
+	public void setPtAttaque(int ptAttaque) {
+		this.ptAttaque = ptAttaque;
+	}
+
+	public int getPvArme() {
+		return pvArme;
+	}
+
+	public void setPvArme(int pvArme) {
+		this.pvArme = pvArme;
+	}
+	
 }
